0b6918c42a69e3aeae8d4687a8e26eb2fd9025e8
ninjinkun <ninjin@mac.com>
2018-10-21 17:03:31 +0900

    refactor: remove react-native-web dependency

--- src/components/pages/FeedView/index.tsx
+++ src/components/pages/FeedView/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
-import { ActivityIndicator } from 'react-native';
 import styled from 'styled-components';
 import firebase from 'firebase/app';
 import 'firebase/auth';
--- src/types/react-native-web.d.ts
+++
@@ -1,7 +0,0 @@
-import { TextProperties } from 'react-native'
-declare module 'react-native' {
-    interface TextProperties {
-      accessibilityRole?: string;
-      href?: string;
-    }
-}
