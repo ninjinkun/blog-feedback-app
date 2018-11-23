48c05813c78a1d8b6d7866c0938eb28fbc3d0b31
ninjinkun <ninjin@mac.com>
2018-11-23 16:42:43 +0900

    feature: add GoogleAuthProvider

--- src/components/pages/AuthPage/index.tsx
+++ src/components/pages/AuthPage/index.tsx
@@ -65,7 +65,7 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
   ]
 };
 
