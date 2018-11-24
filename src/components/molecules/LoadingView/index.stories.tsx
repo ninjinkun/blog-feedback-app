754eda10526215e8466783366d0fd55385cac8eb
ninjinkun <ninjin@mac.com>
2018-11-24 21:01:44 +0900

    feature: add LoadingView storybook

---
+++ src/components/molecules/LoadingView/index.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import LoadingView from './index';
+
+storiesOf('molecules/LoadingView', module).add('default', () => <LoadingView />);
