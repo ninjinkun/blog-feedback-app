4756bfb2b513426fcbeef736fc25ab49aaa89d84
ninjinkun <ninjin@mac.com>
2018-12-01 17:06:00 +0900

    feature: support non UTF-8 text encoding add charset detection and converter to Cloud Functions fetcher

--- functions/src/fetcher.ts
+++ functions/src/fetcher.ts
@@ -1,6 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
+import * as iconv from 'iconv-lite';
+import * as jschardet from 'jschardet';
+const charset = require('charset');
 
 export async function fetch(url: string): Promise<string> {
-  const res: AxiosResponse<string> = await axios.get(url);
-  return res.data;
+  const res: AxiosResponse<Buffer> = await axios.get(url, {      
+     responseType: 'arraybuffer',
+  });
+  let encoding: string = charset(res.headers);
+  if (!encoding) {
+    const text = res.data.toString('utf8');
+    encoding = jschardet.detect(text).encoding.toLowerCase();;
+  }
+  return iconv.decode(res.data, encoding);
 }
