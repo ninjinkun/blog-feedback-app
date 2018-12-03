0dd9ada2a4a91af1ee693eda59d359f5b430a01e
ninjinkun <ninjin@mac.com>
2018-12-04 07:32:43 +0900

    feature: add items test case

--- src/firebase-rules.test.ts
+++ src/firebase-rules.test.ts
@@ -1,8 +1,9 @@
 import * as firebase from '@firebase/testing';
 import { auth } from 'firebase';
 import fs from 'fs';
-import { db as firebaseDB, serverTimestamp } from './models/repositories/app-repository';
+import { db as firebaseDB, serverTimestamp, writeBatch } from './models/repositories/app-repository';
 import { blogRef, saveBlog } from './models/repositories/blog-repository';
+import { saveItemBatch } from './models/repositories/item-repository';
 import { userRef } from './models/repositories/user-repository';
 
 const projectIdBase = 'firestore-emulator-example-' + Date.now();
@@ -28,7 +29,7 @@ serverTimestamp.mockReturnValue(firebase.firestore.FieldValue.serverTimestamp())
 
 const rules = fs.readFileSync('firestore.rules', 'utf8');
 
-beforeEach(() => {
+beforeAll(() => {
   testNumber++;
   return firebase.loadFirestoreRules({
     projectId: getProjectId(),
@@ -36,56 +37,80 @@ beforeEach(() => {
   });
 });
 
-afterEach(() => {
-  return Promise.all(firebase.apps().map(app => app.delete()));
+afterAll(() => Promise.all(firebase.apps().map(app => app.delete())));
+
+describe('/users', () => {
+  it('save unauthorized user', async () => {
+    const db = authedApp(undefined);
+    firebaseDB.mockReturnValue(db);
+    const user = userRef('ninjinkun');
+    await firebase.assertFails(user.set({ birthday: 'January 1' }));
+  });
+
+  it('save to invalid collection', async () => {
+    const db = authedApp({ uid: 'ninjinkun' });
+    await firebase.assertFails(db.doc('test/ninjinkun').set({ hoge: 'fuga' }));
+  });
 });
 
-it('save unauthorized user', async () => {
-  const db = authedApp(undefined);
-  firebaseDB.mockReturnValue(db);
-  const user = userRef('ninjinkun');
-  await firebase.assertFails(user.set({ birthday: 'January 1' }));
+describe('/users/:user_id/blogs', () => {
+  it('save exepct blogs field', async () => {
+    const db = authedApp({ uid: 'ninjinkun' });
+    firebaseDB.mockReturnValue(db);
+    const user = userRef('ninjinkun');
+
+    await firebase.assertFails(user.set({ hoge: 'fuga' }));
+  });
+
+  it('save blog', async () => {
+    const db = authedApp({ uid: 'ninjinkun' });
+    firebaseDB.mockReturnValue(db);
+
+    await firebase.assertSucceeds(
+      saveBlog(
+        'ninjinkun',
+        'https://ninjinkun.hatenablog.com/',
+        "ninjinkun's diary",
+        'https://ninjinkun.hatenablog.com/feed',
+        'atom'
+      )
+    );
+  });
+
+  it('save other users blog', async () => {
+    const db = authedApp({ uid: 'daikonkun' });
+    firebaseDB.mockReturnValue(db);
+
+    await firebase.assertFails(
+      saveBlog(
+        'ninjinkun',
+        'https://ninjinkun.hatenablog.com/',
+        "ninjinkun's diary",
+        'https://ninjinkun.hatenablog.com/feed',
+        'atom'
+      )
+    );
+  });
 });
 
-it('save to invalid collection', async () => {
-  const db = authedApp({ uid: 'ninjinkun' });
-  await firebase.assertFails(db.doc('test/ninjinkun').set({ hoge: 'fuga' }));
-});
+describe('/users/:user_id/blogs/:blog_id/items', () => {
+  it('save items', async () => {
+    const db = authedApp({ uid: 'ninjinkun' });
+    firebaseDB.mockReturnValue(db);
+    writeBatch.mockReturnValue(db.batch());
 
-it('save exepct blogs field', async () => {
-  const db = authedApp({ uid: 'ninjinkun' });
-  firebaseDB.mockReturnValue(db);
-  const user = userRef('ninjinkun');
-
-  await firebase.assertFails(user.set({ hoge: 'fuga' }));
-});
-
-it('save blog', async () => {
-  const db = authedApp({ uid: 'ninjinkun' });
-  firebaseDB.mockReturnValue(db);
-
-  await firebase.assertSucceeds(
-    saveBlog(
+    const batch = writeBatch();
+    saveItemBatch(
+      batch,
       'ninjinkun',
       'https://ninjinkun.hatenablog.com/',
-      "ninjinkun's diary",
-      'https://ninjinkun.hatenablog.com/feed',
-      'atom'
-    )
-  );
-});
+      'https://ninjinkun.hatenablog.com/entry/12345',
+      'Developing blog-feedback-app',
+      new Date(),
+      {},
+      {}
+    );
 
-it('save other users blog', async () => {
-  const db = authedApp({ uid: 'daikonkun' });
-  firebaseDB.mockReturnValue(db);
-
-  await firebase.assertFails(
-    saveBlog(
-      'ninjinkun',
-      'https://ninjinkun.hatenablog.com/',
-      "ninjinkun's diary",
-      'https://ninjinkun.hatenablog.com/feed',
-      'atom'
-    )
-  );
+    await firebase.assertSucceeds(batch.commit());
+  });
 });
