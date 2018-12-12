743640978d7a78c8994cf54ad6e143377b8f57e4
ninjinkun <ninjin@mac.com>
2018-12-12 19:41:11 +0900

    feature: use Service Worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { register } from './serviceWorker';
 
 // App registration and rendering
 render(<App />, document.getElementById('root'));
+register();
