dafd9cda4799d2dfc1d8a81f2466c3402531f43f
ninjinkun <ninjin@mac.com>
2018-12-14 08:44:34 +0900

    fix: ga

--- src/ga.ts
+++ src/ga.ts
@@ -1,9 +1,17 @@
 import ReactGA from 'react-ga';
 
 export function initializeGoogleAnalytics() {
-  if (process.env.NODE_ENV === 'production') {
-    ReactGA.initialize('UA-36926308-2');
-  } else {
-    ReactGA.initialize('');
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      ReactGA.initialize('UA-36926308-2');
+      break;
+    case 'development':
+      ReactGA.initialize('', { debug: true });
+      break;
+    case 'test':
+      ReactGA.initialize('', { testMode: true });
+      break;
+    default:
+      break;
   }
 }
