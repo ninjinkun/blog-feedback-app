9fcd25ae3bee42313f9c0e41117646dc521e9811
ninjinkun <ninjin@mac.com>
2018-12-14 01:35:13 +0900

    fix: surpress GA initalization log

--- src/ga.ts
+++ src/ga.ts
@@ -3,5 +3,7 @@ import ReactGA from 'react-ga';
 export function initializeGoogleAnalytics() {
   if (process.env.NODE_ENV === 'production') {
     ReactGA.initialize('UA-36926308-2');
+  } else {
+    ReactGA.initialize('');
   }
 }
