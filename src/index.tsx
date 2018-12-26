3954f17a75794a3b8ecc88cdf70a10a02e94b803
ninjinkun <ninjin@mac.com>
2018-12-27 08:10:54 +0900

    feature: kill Service Worker in Safari and Mobile Safari

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
+import UAParser from 'ua-parser-js';
+
 import App from './App';
-import { register } from './serviceWorker';
+import { register, unregister } from './serviceWorker';
 
 // App registration and rendering
 render(<App />, document.getElementById('root'));
-register();
+
+const parser = new UAParser();
+const result = parser.getResult();
+// Safari's Service Woker sometimes caches HTML as JavaScript
+const isSafari = result.browser.name === 'Safari' || result.browser.name === 'Mobile Safari';
+if (isSafari) {
+  unregister();
+} else {
+  register();
+}
