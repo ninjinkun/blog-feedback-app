f26534c268985403767f7358543b8fb47161c7fe
ninjinkun <ninjin@mac.com>
2019-01-04 14:00:55 +0900

    feature: welcome mail

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
+import * as firebase from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { fetchText } from './fetcher';
+import { sendWelcomeMail as sendWelcomeMailAction } from './send-welcome-mail';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -10,4 +12,14 @@ export const crossOriginFetch = functions.region('asia-northeast1').https.onCall
   const { url } = data;
   const res = await fetchText(url);
   return { body: res };
+});
+
+export const sendWelcomeMailLocal = functions.region('asia-northeast1').https.onCall(async (data, context) => {
+  await sendWelcomeMailAction('ninjinkun@gmail.com');
+  return true;
+});
+
+export const sendWelcomeMail = functions.region('asia-northeast1').auth.user().onCreate(async (user, context) => {
+  await sendWelcomeMailAction(user.email);
+  return true;
 });
---
+++ functions/src/send-welcome-mail.ts
@@ -0,0 +1,19 @@
+import { transport } from './mail-transport';
+import EmailTemplate = require('email-templates');
+
+export function sendWelcomeMail(to: string) {
+  const email = new EmailTemplate({
+    message: {
+      from: 'info@blog-feedback.app'
+    }, 
+    transport: { jsonTransport: true },
+  });
+
+  return email.send({
+    template: 'welcome',
+    message: {
+      to,
+    },
+    locals: {},
+  });
+}
