af350268048a1ed375f7c8f7d4130307756e1583
ninjinkun <ninjin@mac.com>
2019-02-11 19:57:12 +0900

    feature: use hooks

--- src/components/molecules/HeaderLoadingIndicator/index.stories.tsx
+++ src/components/molecules/HeaderLoadingIndicator/index.stories.tsx
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import HeaederLoadingIndicator from './index';
 
@@ -9,56 +9,49 @@ type Item = {
   loading: boolean;
 };
 
-class Test extends React.PureComponent<{}, { item: Item }> {
-  items = [
-    {
-      label: '',
-      ratio: 0,
-      loading: false,
-    },
-    {
-      label: 'はてなブックマークを読み込んでいます',
-      ratio: 20,
-      loading: true,
-    },
-    {
-      label: 'Facebookを読み込んでいます',
-      ratio: 50,
-      loading: true,
-    },
-    {
-      label: 'Pocketを読み込んでいます',
-      ratio: 80,
-      loading: true,
-    },
-    {
-      label: '',
-      ratio: 100,
-      loading: false,
-    },
-  ];
-  index = 0;
-  state = { item: this.items[0] };
-  constructor(props: any) {
-    super(props);
-    this.clickButton = this.clickButton.bind(this);
-  }
-  clickButton() {
-    this.index = this.index === this.items.length - 1 ? 0 : this.index + 1;
-    this.setState({ item: this.items[this.index] });
-  }
-  render() {
-    return (
-      <Wrapper onClick={this.clickButton}>
-        <HeaederLoadingIndicator
-          ratio={this.state.item.ratio}
-          label={this.state.item.label}
-          loading={this.state.item.loading}
-        />
-      </Wrapper>
-    );
-  }
-}
+const initialItems = [
+  {
+    label: '',
+    ratio: 0,
+    loading: false,
+  },
+  {
+    label: 'はてなブックマークを読み込んでいます',
+    ratio: 20,
+    loading: true,
+  },
+  {
+    label: 'Facebookを読み込んでいます',
+    ratio: 50,
+    loading: true,
+  },
+  {
+    label: 'Pocketを読み込んでいます',
+    ratio: 80,
+    loading: true,
+  },
+  {
+    label: '',
+    ratio: 100,
+    loading: false,
+  },
+];
+
+const Test: React.FC = props => {
+  const [index, setIndex] = useState<number>(0);
+  const [item, setItems] = useState<Item>(initialItems[index]);
+
+  const clickButton = () => {
+    setIndex(index === initialItems.length - 1 ? 0 : index + 1);
+    setItems(initialItems[index]);
+  };
+
+  return (
+    <Wrapper onClick={clickButton}>
+      <HeaederLoadingIndicator ratio={item.ratio} label={item.label} loading={item.loading} />
+    </Wrapper>
+  );
+};
 
 storiesOf('molecules/HeaederLoadingIndicator', module).add('default', () => <Test />);
 
