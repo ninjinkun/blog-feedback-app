7944209dfb7725cb6eaf5187bc2acd6fe12f8dc7
ninjinkun <ninjin@mac.com>
2019-04-13 13:52:57 +0900

    fix: import interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
-import UAParser from 'ua-parser-js';
+import { UAParser } from 'ua-parser-js';
 
 import App from './App';
 import { register, unregister } from './serviceWorker';
