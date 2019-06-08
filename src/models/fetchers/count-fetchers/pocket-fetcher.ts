acbcb3ac25a903d6d4423d981ae4dffa35d74926
ninjinkun <ninjin@mac.com>
2019-06-08 10:12:19 +0900

    feature: new pocket api

--- src/models/fetchers/count-fetchers/pocket-fetcher.ts
+++ src/models/fetchers/count-fetchers/pocket-fetcher.ts
@@ -3,16 +3,9 @@ import { CountResponse } from '../../responses';
 import { crossOriginFetch } from '../functions';
 
 export async function fetchPocketCount(url: string): Promise<CountResponse> {
-  const apiURL = `https://widgets.getpocket.com/v1/button?label=pocket&count=vertical&v=1&url=${encodeURIComponent(
-    url
-  )}&src=${encodeURIComponent(url)}`;
+  const apiURL = `https://widgets.getpocket.com/api/saves?url=${encodeURIComponent(url)}`;
   const response = await crossOriginFetch(apiURL);
-  const htmlText = response.data.body;
-
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlText, 'text/html');
-  const result = doc.evaluate(`//em[@id="cnt"]/text()`, doc, null, XPathResult.STRING_TYPE, null);
-  const countString = result.stringValue;
-  const count = countString ? parseInt(countString.replace(/,/, ''), 10) : 0;
+  const json = JSON.parse(response.data.body);
+  const count = json.saves;
   return { url, count, type: CountType.Pocket };
 }
