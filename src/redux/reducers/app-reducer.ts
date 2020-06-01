d0f856aac65b94b059d4ba83dc82392ced37cd37
ninjinkun <ninjin@mac.com>
2020-06-02 08:45:55 +0900

    refactor: move from redux to redux-toolkit (unfinished)

--- src/redux/reducers/app-reducer.ts
+++ src/redux/reducers/app-reducer.ts
@@ -4,11 +4,11 @@ import { blogReducer } from './blog-reducer';
 import { deleteBlogReducer } from './delete-blog-reducer';
 import { feedsReducer } from './feed-reducer';
 import { settingsReducer } from './setting-reducer';
-import { userReducer } from './user-reducer';
+import { userSlice } from '../states/user-state';
 
 export const appReducer = combineReducers({
   blog: blogReducer,
-  user: userReducer,
+  user: userSlice.reducer,
   addBlog: addBlogReducer,
   deleteBlog: deleteBlogReducer,
   feeds: feedsReducer,
--- src/redux/states/user-state.ts
+++ src/redux/states/user-state.ts
@@ -1,3 +1,4 @@
+import { createSlice, ThunkAction } from '@reduxjs/toolkit';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -7,3 +8,74 @@ export type UserState = {
 };
 
 export const initialState: UserState = { loading: true };
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    firebaseUserRequest(state) {
+      return { ...state, loading: true };
+    },
+    firebaseUserResponse(state, action: { payload: firebase.User }) {
+      return { ...state, user: action.payload, loading: false };
+    },
+    firebaseUnauthorizedError(state) {
+      return { ...state, loading: false };
+    },
+    firebaseSignoutResponse(state) {
+      return { ...initialState, loading: false };
+    },
+    firebaseSignoutRequest(state) {
+      return state;
+    },
+    firebaseSignoutError(state, action: { payload: Error }) {
+      return state;
+    },
+  },
+});
+
+export function fetchUser(auth: firebase.auth.Auth): ThunkAction<void, UserState, undefined, any> {
+  return async (dispatch) => {
+    console.log('hogehogheogheo!');
+    try {
+      dispatch(userSlice.actions.firebaseUserRequest());
+      const user = await currenUserOronAuthStateChanged(auth);
+      dispatch(userSlice.actions.firebaseUserResponse(user));
+    } catch (e) {
+      dispatch(userSlice.actions.firebaseUnauthorizedError());
+    }
+  };
+}
+
+export function onAuthStateChanged(auth: firebase.auth.Auth): Promise<firebase.User> {
+  return new Promise((resolve, reject) => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        resolve(user);
+      } else {
+        reject(new Error('User login failed'));
+      }
+    });
+  });
+}
+
+export async function currenUserOronAuthStateChanged(auth: firebase.auth.Auth): Promise<firebase.User> {
+  const currentUser = auth.currentUser;
+  if (currentUser) {
+    return currentUser;
+  } else {
+    return onAuthStateChanged(auth);
+  }
+}
+
+export function signOut(auth: firebase.auth.Auth): ThunkAction<void, UserState, undefined, any> {
+  return async (dispatch) => {
+    try {
+      dispatch(userSlice.actions.firebaseSignoutRequest());
+      await auth.signOut();
+      dispatch(userSlice.actions.firebaseSignoutResponse());
+    } catch (e) {
+      dispatch(userSlice.actions.firebaseSignoutError(e));
+    }
+  };
+}
