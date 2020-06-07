f29d68dae43bd85bf10fb99198a0f3d36b2883eb
ninjinkun <ninjin@mac.com>
2020-06-07 16:00:28 +0900

    refactor: refine AddBlogPage with redux-toolkit

--- src/redux/reducers/app-reducer.ts
+++ src/redux/reducers/app-reducer.ts
@@ -1,15 +1,15 @@
 import { combineReducers } from 'redux';
-import { addBlogReducer } from './add-blog-reducer';
 import { settingsReducer } from './setting-reducer';
 import { userSlice } from '../states/user-state';
 import { blogSlice } from '../states/blog-state';
 import { deleteBlogSlice } from '../states/delete-blog-state';
 import { feedsSlice } from '../states/feeds-state';
+import { addBlogSlice } from '../states/add-blog-state';
 
 export const appReducer = combineReducers({
   blog: blogSlice.reducer,
   user: userSlice.reducer,
-  addBlog: addBlogReducer,
+  addBlog: addBlogSlice.reducer,
   deleteBlog: deleteBlogSlice.reducer,
   feeds: feedsSlice.reducer,
   settings: settingsReducer,
