fe26a571c40153bd1dd2ed3401cc48c7ac925fa4
ninjinkun <ninjin@mac.com>
2022-03-23 11:39:10 +0900

    fix: remove useless escapes

--- src/utils/canonicalize.ts
+++ src/utils/canonicalize.ts
@@ -1,8 +1,8 @@
 export function canonicalize(url: string) {
   // Firebase hosting on production and web browsers return single slash protocol part such as "http:/"
-  if (/^http:\/[^\/]/.test(url)) {
+  if (/^http:\/[^/]/.test(url)) {
     return url.replace(/^http:\//, 'http://');
-  } else if (/^https:\/[^\/]/.test(url)) {
+  } else if (/^https:\/[^/]/.test(url)) {
     return url.replace(/^https:\//, 'https://');
   } else {
     return url;
